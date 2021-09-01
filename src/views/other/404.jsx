db32d3f92c2415afe23b1b56df52c52a61cfa5b1
Edward <edward.wjh@link8tech.tw>
2021-09-01 17:26:33 +0800

    所需頁面建置

---
+++ src/views/other/404.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function View404(props) {
+  return <div>404</div>
+}
+
+export default View404
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {},
+    },
   },
   variants: {
     extend: {},
