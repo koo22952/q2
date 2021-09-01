e33a5d9ce0e9b7a14e4b88c6015df484191c800a
Edward <edward.wjh@link8tech.tw>
2021-09-01 17:29:47 +0800

    feat: login form test

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
 function Login(props) {
-  return <div>Login</div>
+  const handleSubmit = (e) => {
+    console.log(e)
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">帳號：</label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          autoComplete="username"
+          placeholder="請輸入帳號"
+        />
+        <label htmlFor="password">密碼：</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          placeholder="請輸入密碼"
+        />
+        <button type="submit">送出</button>
+      </form>
+    </div>
+  )
 }
 
 export default Login
