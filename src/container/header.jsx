8e42aa948a1d58ebc9abcf0b3aa26db89a3ff1f3
Edward <koo22952@gmail.com>
2021-09-05 02:09:19 +0800

    feat: header版面設置

--- src/container/header.jsx
+++ src/container/header.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 function Header(props) {
-  return <div>頁面的表頭</div>
+  return (
+    <header className="flex items-center justify-between bg-white px-4 py-2 border-b">
+      <div className="font-bold text-2xl">LOGO</div>
+      <ul className="flex items-center text-sm text-blue-500">
+        <li className="mr-4">
+          <Link to="/">首頁</Link>
+        </li>
+        <li>
+          <Link to="/news">最新消息</Link>
+        </li>
+      </ul>
+    </header>
+  )
 }
 
 export default Header
