637e15bafb429b4991d0ce3e603495f2fb3c40a2
Edward <koo22952@gmail.com>
2021-09-06 00:37:42 +0800

    feat: axios 校驗設置

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import Input from '../../components/input'
+import axios from 'axios'
 
 function Login(props) {
   const [form, setForm] = useState({})
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     console.log(form)
+
+    await axios
+      .post('/api/login', {
+        username: '1123',
+        password: '123132',
+      })
+      .then((res) => {
+        console.log(res)
+      })
+
     e.preventDefault()
   }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-510	p-8 m-8  bg-white shadow-md rounded-md">
+      <div className="w-520	p-8 m-8  bg-white shadow-md rounded-md">
         <div className="font-bold text-2xl text-center mb-4">登入</div>
         <Input
           className="mb-4"
           label="帳號"
-          htmlType="account"
+          name="account"
           InputType="text"
           placeholder="請輸入帳號"
           setForm={setForm}
@@ -25,7 +36,7 @@ function Login(props) {
         <Input
           className="mb-4"
           label="密碼"
-          htmlType="password"
+          name="password"
           InputType="password"
           placeholder="請輸入密碼"
           showEye
