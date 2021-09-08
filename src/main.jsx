f121ec474f29847946d7538f27d3d82a80323b6c
Edward <edward.wjh@link8tech.tw>
2021-09-08 16:25:01 +0800

    fix: 移除msw相關

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import { setupMSW } from './mocks/browser'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-setupMSW().then(() => ReactDOM.render(<App />, document.getElementById('root')))
+ReactDOM.render(<App />, document.getElementById('root'))
--- src/mocks/browser.js
+++
@@ -1,7 +0,0 @@
-import { setupWorker } from "msw";
-import { handlers } from "./handlers";
-
-export const setupMSW = async (options = {}) => {
-  const worker = setupWorker(...handlers);
-  await worker.start();
-};
