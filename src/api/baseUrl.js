59b11023a636952cca7aff3de8dc4f61e258a67d
Edward <edward.wjh@link8tech.tw>
2021-09-08 16:06:57 +0800

    feat: api前後攔 & api取得路徑建置

---
+++ src/api/baseUrl.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+export const request = axios.create({
+  baseURL: '/api',
+  timeout: 5000,
+})
+
+// 添加請求攔截器
+request.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('userToken')
+
+    if (token !== null) {
+      config.headers['Content-Type'] = 'application/json'
+      config.headers['token'] = token
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+request.interceptors.response.use(
+  (response) => {
+    if (response.status === 200) {
+      return Promise.resolve(response)
+    } else {
+      return Promise.reject(response)
+    }
+  },
+  (error) => {
+    // 相對應錯誤處理
+    switch (error.response?.status) {
+      case 401:
+      case 403:
+      case 500:
+      default:
+        console.log('就是發生了錯誤！')
+    }
+
+    return Promise.reject(error)
+  }
+)
