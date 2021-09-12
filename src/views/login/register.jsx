aec85b0e820c3b268202d87d5c7f6993073b8a62
Edward <koo22952@gmail.com>
2021-09-12 23:18:22 +0800

    feat: 添加使用者名稱欄位

--- src/views/login/register.jsx
+++ src/views/login/register.jsx
@@ -14,6 +14,7 @@ function Register(props) {
   const history = useHistory()
   const [form, setForm] = useState({
     username: '',
+    name: '',
     password: '',
     rePassword: '',
   })
@@ -109,28 +110,38 @@ function Register(props) {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-520	p-8 m-8 bg-white shadow-md rounded-md">
-        <div className="font-bold text-2xl text-center mb-4">註冊</div>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <div className='w-520	p-8 m-8 bg-white shadow-md rounded-md'>
+        <div className='font-bold text-2xl text-center mb-4'>註冊</div>
         <Input
-          className="mb-4"
-          label="帳號"
-          labelClassName="w-16"
-          name="username"
-          InputType="email"
-          placeholder="必須是信箱"
+          className='mb-4'
+          label='帳號'
+          labelClassName='w-20'
+          name='username'
+          InputType='email'
+          placeholder='必須是信箱'
           required
           onChange={onChange}
           isUse={form.username?.isUse || false}
           verify={form.username?.verify}
         />
         <Input
-          className="mb-4"
-          label="密碼"
-          labelClassName="w-16"
-          name="password"
-          InputType="password"
-          placeholder="4-8字元；首尾必須是英文；中間必須是數字"
+          className='mb-4'
+          label='使用者名稱'
+          labelClassName='w-20'
+          name='name'
+          InputType='text'
+          placeholder='可選，對其他用戶顯示的名稱'
+          onChange={onChange}
+          isUse={false}
+        />
+        <Input
+          className='mb-4'
+          label='密碼'
+          labelClassName='w-20'
+          name='password'
+          InputType='password'
+          placeholder='4-8字元；首尾必須是英文；中間必須是數字'
           showEye
           required
           onChange={onChange}
@@ -138,26 +149,26 @@ function Register(props) {
           verify={form.password?.verify}
         />
         <Input
-          className="mb-4"
-          label="確認密碼"
-          labelClassName="w-16"
-          name="rePassword"
-          InputType="password"
-          placeholder="4-8字元；首尾必須是英文；中間必須是數字"
+          className='mb-4'
+          label='確認密碼'
+          labelClassName='w-20'
+          name='rePassword'
+          InputType='password'
+          placeholder='4-8字元；首尾必須是英文；中間必須是數字'
           showEye
           required
           onChange={onChange}
           isUse={form.rePassword?.isUse || false}
           verify={form.rePassword?.verify}
         />
-        <div className="text-center mt-6">
-          <div className="text-center mb-4">
-            <Link className="text-blue-500 text-sm" to={'/login'}>
+        <div className='text-center mt-6'>
+          <div className='text-center mb-4'>
+            <Link className='text-blue-500 text-sm' to={'/login'}>
               返回登入
             </Link>
           </div>
           <button
-            className="px-8 py-2 bg-blue-700 rounded-md text-white hover:bg-blue-800"
+            className='px-8 py-2 bg-blue-700 rounded-md text-white hover:bg-blue-800'
             onClick={onSubmit}
           >
             註冊
